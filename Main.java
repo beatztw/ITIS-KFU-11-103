a7e3544cac1d059d6c131cc143b6f8ec088bdae5
Aleksey Chugunov <alex.160404@mail.com>
2021-09-15 18:51:16 +0300

    Second commit

--- Main.java
+++ Main.java
@@ -1,21 +1,30 @@
+import java.util.Scanner;
 public class Main{
 	public static void main (String args[]){
-		if(args.length < 4){
-			System.out.println("4 args reqired");
-			return;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Vvedite koord");
+		double x = scanner.nextDouble();
+		double y = scanner.nextDouble();
+		if( x >= 0 && x <= 1 && y >= 0 & y <= 1) {
+			System.out.println("Ne vhodit");
+		} else {
+			System.out.println("Vhodit");
 		}
-		//float[] x = {1.f,2.f,3.f};
-		//float sum = 0.f;
-		//for(int i = 0; i<3; i++){
-		//	sum += x[i];
-		//}
-		//System.out.println(sum);
-		float x1 = Float.value0(args[0]);
-		float y1 = Float.value0(args[1]);
-		float x2 = Float.value0(args[2]);
-		float y2 = Float.value0(args[3]);
 		
-		float distance = (float) Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * ( y1 - y2));
-		System.out.println(distance);
 	}
 }
+
+
+
+
+//double sum = 0;
+	//	double number = -1;
+		//int count = 0;
+		//while (true) {
+			//number = scanner.nextDouble();
+			//if (number == 0) {
+				//break;
+			//}
+			//count++;
+			//sum += number;
+			//System.out.println("Average: " + sum / count);
