a9d4a311259e181782d1e2cacd387ba737d1053c
Alexsey Chugunov <43906446+beatztw@users.noreply.github.com>
2022-05-10 11:51:30 +0300

    Termination of withdrawal by enter

---
+++ Threads.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Threads {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Runnable task0 = () -> {
+            try {
+                while(!Thread.interrupted()) {
+                    System.out.println("TEXT MESSAGE");
+                    TimeUnit.MILLISECONDS.sleep(700);
+                }
+            } catch (InterruptedException e) {
+            }
+        };
+
+        Thread thread0 = new Thread(task0);
+        thread0.start();
+
+        String enterKey = scanner.nextLine();
+        if(enterKey == ""){
+            thread0.interrupt();
+        }
+    }
+}
+
